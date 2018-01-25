0c1be6ec4da07132b2a86d1ddd8ef9302a83a2cf
SametSahin10 <a.sahin.ual@gmail.com>
2018-01-26 02:01:12 +0300

    Add files via upload

---
+++ CodingBat/WarmUp1/diff21/Diff21.java
@@ -0,0 +1,36 @@
+package diff21;
+
+/**
+ *Given an int n, return the absolute difference between n and 21, 
+ *except return double the absolute difference if n is over 21.
+ */
+
+
+
+/**
+ *
+ * @author sametsahin
+ */
+public class Diff21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {}
+    
+    
+    public static int diff21(int n){
+        
+        if (n > 21) {
+            
+            return 2 * (Math.abs(n - 21));
+            
+        }else{
+            
+            return Math.abs(n - 21);
+            
+        }
+        
+    }
+    
+}
