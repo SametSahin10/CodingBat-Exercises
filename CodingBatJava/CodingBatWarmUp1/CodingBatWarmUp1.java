c88d41c243e5de08fd83e1f6ba6fe3cda9a30eb1
Enes Demirağ <enesdemirag1@hotmail.com>
2018-01-25 17:23:39 +0300

    Delete unnecessary files

---
+++ CodingBatJava/CodingBatWarmUp1/CodingBatWarmUp1.java
@@ -0,0 +1,8 @@
+﻿public class CodingBatWarmUp1 {
+
+    public static void main(String[] args) {}
+
+    public boolean sleepIn(boolean weekday, boolean vacation){   
+        return weekday == false || vacation == true;     
+    }
+}
