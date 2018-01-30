fcb9e0a420a77a60f197388e67d32f1f2ce1aec0
SametSahin10 <a.sahin.ual@gmail.com>
2018-01-30 21:08:53 +0300

    Add files via upload

---
+++ CodingBat/WarmUp1/makes10/Makes10.java
@@ -0,0 +1,35 @@
+package makes10;
+
+/*
+* Given 2 ints, a and b, return true if one of them is 10 or if their sum is 10.
+
+
+* makes10(9, 10) → true
+* makes10(9, 9) → false
+* makes10(1, 9) → true
+*/
+
+
+/**
+ *
+ * @author sametsahin
+ */
+
+public class Makes10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+    public boolean makes10(int a, int b){
+        
+        return (a == 10) || (b == 10) || (a + b == 10);
+        
+    }
+    
+}
